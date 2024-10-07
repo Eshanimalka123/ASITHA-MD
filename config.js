589bd63044b03e17eb03ac1e432e847b113b85b0
Eshanimalka123 <Eshanimalka432@gmail.com>
2024-10-07 16:47:34 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=bIEWmIgZ#gdjDA5p0HPb7jgQOFBKi6d-ZNZ51K_7g3A1RrsEQZhc",
 MONGODB: process.env.MONGODB || "",//enter mongo db url
 };
